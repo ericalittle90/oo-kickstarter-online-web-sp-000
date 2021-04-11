ff522833fe46e29bf3288cf59a6ccb1589d1fe17
Erica Little <eteney90@gmail.com>
2021-04-11 14:30:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
 
-  attr_reader :backers, :name
+  attr_reader :backers, :title
 
-  def initialize(name)
+  def initialize(title)
     @backers = []
-    @name = name 
+    @title = title
   end
 
   def add_backer(backer)
