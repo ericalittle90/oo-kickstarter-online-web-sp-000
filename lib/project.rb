59864214136db240848641ae889e1416683e5e13
Erica Little <eteney90@gmail.com>
2021-04-11 14:31:19 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,11 +4,11 @@ class Project
 
   def initialize(title)
     @backers = []
-    @title = title
+    @title = title 
   end
 
   def add_backer(backer)
-    @backers << self
+    @backers << backer 
   end
 
 end
